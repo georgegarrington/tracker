575347b03720dab4cf5a61401f2861616c4691b4
George Garrington <georgewgarrington@gmail.com>
2025-12-04 22:41:08 +0000

    OpenAPI POC

--- backend/src/tracker/app.py
+++ backend/src/tracker/app.py
@@ -1,21 +1,29 @@
 
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
-from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
-async def test_route():
-    return JSONResponse({'key': 'val'})
+from tracker.utils.fastapi._make_route import make_route
+
+class Person(BaseModel):
+    name: str
+    age: int
+
+
+async def test_route() -> Person:
+    return Person(
+        name="George",
+        age=28,
+    )
+
 
 def make_service() -> FastAPI:
-
     return FastAPI(
-        routes = [
-            APIRoute(
-                path = "/v1/test",
-                methods = ["GET"],
-                endpoint = test_route
+        routes=[
+            make_route(
+                path="/v1/test",
+                method="GET",
+                endpoint=test_route,
             )
         ]
     )
-
-    pass
---
+++ backend/src/tracker/utils/fastapi/_make_route.py
@@ -0,0 +1,16 @@
+from typing import Callable, Literal, Any, get_type_hints
+from fastapi.routing import APIRoute
+
+
+def make_route(
+    path: str,
+    method: Literal["GET", "POST"],
+    endpoint: Callable[..., Any],
+) -> APIRoute:
+    
+    return APIRoute(
+        path = path,
+        methods = [method],
+        endpoint = endpoint,
+        response_model=get_type_hints(endpoint).get("return"),
+    )
