9207951b14a281c320ceed644ff80b04ac4ff56c
George Garrington <georgewgarrington@gmail.com>
2025-12-14 15:12:19 +0000

    POC of routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,7 @@
 // import Link from '@mui/material/Link';
-import { AppBar, Box, Container, CssBaseline, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, ThemeProvider, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, CssBaseline, Drawer, List, ListItem, ListItemButton, ListItemText, ThemeProvider, Toolbar, Typography } from '@mui/material';
 import { THEME } from './theme';
-import { Dashboard } from '@mui/icons-material';
-import { Route, Routes, RouterLink } from 'react-router-dom';
-
-// function Copyright() {
-//   return (
-//     <Typography
-//       variant="body2"
-//       align="center"
-//       sx={{
-//         color: 'text.secondary',
-//       }}
-//     >
-//       {'Copyright © '}
-//       <Link color="inherit" href="https://mui.com/">
-//         Your Website
-//       </Link>{' '}
-//       {new Date().getFullYear()}.
-//     </Typography>
-//   );
-// }
+import { Route, Routes, Link } from 'react-router-dom';
 
 const drawerWidth = 240;
 
@@ -33,47 +14,42 @@ export default function App() {
         <AppBar position="fixed" sx={{backgroundColor: "primary.main", zIndex: theme => theme.zIndex.drawer + 1}}>
           <Toolbar variant='dense'>
             <Typography variant="h6" color="text.secondary" noWrap>
-              {/* The quick brown fox jumps over the lazy dog */}
               Tracker
             </Typography>
           </Toolbar>
         </AppBar>
         <Drawer variant="permanent" anchor="left" sx={{ width: drawerWidth, '& .MuiDrawer-paper': { width: drawerWidth } }}>
           <Toolbar />
-          {/* <Box sx={{ width: 240, bgcolor: 'red', height: '100%' }}> */}
-          {/* <List>
+          <List>
             <ListItem key={1}>
-              <ListItemButton>
-                <ListItemIcon>
-                  <Dashboard />
-                </ListItemIcon>
-                <ListItemText primary={"Dashboard"} />
+              <ListItemButton
+              component={Link}
+              to="/roles"
+              >
+                <ListItemText primary={"Roles"} />
               </ListItemButton>
             </ListItem>
             <ListItem key={2}>
               <ListItemButton
-              component={RouterLink}
+              component={Link}
+              to="/coding"
               >
-                <ListItemIcon>
-                  <Dashboard />
-                </ListItemIcon>
-                <ListItemText primary={"Dashboard"} />
+                <ListItemText primary={"Coding"} />
               </ListItemButton>
             </ListItem>
-          </List> */}
+          </List>
           
-          {/* </Box> */}
         </Drawer>
-{/* 
-        <Box component="main" sx={{ flexGrow: 1, p: 3, ml: `${drawerWidth}px` }}>
-          <Toolbar />
-          <Routes>
-            <Route path="/one" element={<div>One</div>} />
-            <Route path="/two" element={<div>Two</div>} />
-          </Routes>
-        </Box> */}
 
-        {/* <Typography>test</Typography> */}
+
+        <Box sx={{ml: `${drawerWidth}px`, flexGrow: 1, pt: 0}}>
+          <Toolbar variant="dense"/>
+          <Routes>
+            <Route path ="/roles" element={<div>Roles</div>} />
+            <Route path ="/coding" element={<div>Coding</div>} />
+          </Routes>
+        </Box>
+
       </Box>
       </ThemeProvider>
     </>
