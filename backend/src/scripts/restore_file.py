1f81d36e5c35fae7ce095769c2ce47d72a036c19
George Garrington <georgewgarrington@gmail.com>
2025-12-20 20:41:02 +0000

    Add backup restore script

--- backend/src/scripts/restore_file.py
+++ backend/src/scripts/restore_file.py
@@ -1,6 +1,18 @@
-SQL_BACKUP_PATH = ""
-DEST_SQLITE_PATH = ""
+import sqlite3
+
+SQL_BACKUP_PATH = "/Users/georgegarrington/source/tracker/backup/bkup.sql"
+DEST_SQLITE_PATH = "/Users/georgegarrington/Documents/trackerdb_restored"
 
 
 def restore_file() -> None:
-    pass
+    with open(SQL_BACKUP_PATH, "r") as f:
+        sql = f.read()
+
+    with sqlite3.connect(DEST_SQLITE_PATH) as conn:
+        cursor = conn.cursor()
+        cursor.executescript(sql)
+        conn.commit()
+
+
+if __name__ == "__main__":
+    restore_file()
